0a37faa078c864b26a5df80030c72a521e48411b
Sahil Sharma <sahilsharm1617@gmail.com>
2023-03-18 13:47:19 -0700

    got basic animation working for player

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -4,6 +4,9 @@ const ACCELERATION: int = 15
 const FRICTION: int = 15
 const MAX_SPEED: int = 150
 
+@onready
+var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _physics_process(_delta):
 	var input: Vector2 = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -14,7 +17,16 @@ func _physics_process(_delta):
 		self.velocity = self.velocity.move_toward(input * MAX_SPEED, ACCELERATION)
 		self.velocity.x = clamp(self.velocity.x, -1 * MAX_SPEED, MAX_SPEED)
 		self.velocity.y = clamp(self.velocity.y, -1 * MAX_SPEED, MAX_SPEED)
+		if velocity.x > 0:
+			animation_player.play("run_right")
+		elif velocity.x < 0:
+			animation_player.play("run_left")
+		elif velocity.y < 0:
+			animation_player.play("run_up")
+		elif velocity.y > 0:
+			animation_player.play("run_down")
 	else:
+		animation_player.play("idle_right")
 		self.velocity = self.velocity.move_toward(Vector2.ZERO, FRICTION)
 
 	# move_and_collide(velocity)
