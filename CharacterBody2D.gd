04c0f2440c2e48c9945bb5b97bb47ea6e233a599
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-03-17 22:43:51 -0700

    got movement working with acceleration and deccel

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,15 +1,24 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const ACCELERATION: int = 25
+const FRICTION: int = 25 
+const MAX_SPEED: int = 200
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		self.velocity.x = direction * SPEED
-	else:
-		self.velocity.x = move_toward(self.velocity.x, 0, SPEED)
+	var input: Vector2 = Vector2.ZERO
+	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input = input.normalized()
 
-	# move_and_slide()
+	if input != Vector2.ZERO:
+		velocity += input * ACCELERATION * delta
+		velocity.x = clamp(velocity.x, -1 * MAX_SPEED * delta, MAX_SPEED * delta)
+		velocity.y = clamp(velocity.y, -1 * MAX_SPEED * delta, MAX_SPEED * delta)
+		# velocity.x = clamp(velocity.x, 0, MAX_SPEED * delta)
+		# velocity.y = clamp(velocity.y, 0, MAX_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		# velocity = Vector2.ZERO
+
+	print(velocity)
+	move_and_collide(velocity)
