58d7124185e9f3b51481c9c35376021c696cdca2
Sahil Sharma <sahilsharm1617@gmail.com>
2023-03-18 14:20:25 -0700

    added animation trees and 2d blend states for playing animations

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -5,7 +5,9 @@ const FRICTION: int = 15
 const MAX_SPEED: int = 150
 
 @onready
-var animation_player: AnimationPlayer = $AnimationPlayer
+var animation_tree: AnimationTree = $AnimationTree
+@onready
+var animation_state = animation_tree.get("parameters/playback")
 
 func _physics_process(_delta):
 	var input: Vector2 = Vector2.ZERO
@@ -14,20 +16,14 @@ func _physics_process(_delta):
 	input = input.normalized()
 
 	if input != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input)
+		animation_tree.set("parameters/Run/blend_position", input)
+		animation_state.travel("Run")
 		self.velocity = self.velocity.move_toward(input * MAX_SPEED, ACCELERATION)
 		self.velocity.x = clamp(self.velocity.x, -1 * MAX_SPEED, MAX_SPEED)
 		self.velocity.y = clamp(self.velocity.y, -1 * MAX_SPEED, MAX_SPEED)
-		if velocity.x > 0:
-			animation_player.play("run_right")
-		elif velocity.x < 0:
-			animation_player.play("run_left")
-		elif velocity.y < 0:
-			animation_player.play("run_up")
-		elif velocity.y > 0:
-			animation_player.play("run_down")
 	else:
-		animation_player.play("idle_right")
+		animation_state.travel("Idle")
 		self.velocity = self.velocity.move_toward(Vector2.ZERO, FRICTION)
-
-	# move_and_collide(velocity)
+		
 	self.move_and_slide()
