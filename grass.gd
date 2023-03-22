9e0bd38ebcdfa6e0a531bbf6ada5676964ee7483
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-03-21 22:01:57 -0700

    set up basic hitboxes and hurtboxes

--- grass.gd
+++ grass.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-func _process(_delta):
-	if Input.is_action_just_pressed("attack"):
-		var world: Node = get_tree().current_scene
-		var grass_effect_scene: PackedScene = load("res://GrassEffect.tscn")
-		var grass_effect: Node = grass_effect_scene.instantiate()
-		grass_effect.global_position = self.global_position
-		world.add_child(grass_effect)
-		queue_free()
+func create_grass_effect():
+	var world: Node = get_tree().current_scene
+	var grass_effect_scene: PackedScene = load("res://GrassEffect.tscn")
+	var grass_effect: Node = grass_effect_scene.instantiate()
+	grass_effect.global_position = self.global_position
+	world.add_child(grass_effect)
 	
+func _on_hurtbox_area_entered(_area:Area2D):
+	create_grass_effect()
+	queue_free()
