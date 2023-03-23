fa02a5552bacacafeb6450f59dc0c6c4df5a64dc
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-03-22 22:39:52 -0700

    fixed grass and sword hitbox detection

--- grass.gd
+++ grass.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 func create_grass_effect():
-	var world: Node = get_tree().current_scene
 	var grass_effect_scene: PackedScene = load("res://GrassEffect.tscn")
 	var grass_effect: Node = grass_effect_scene.instantiate()
-	grass_effect.global_position = self.global_position
+	var world: Node = get_tree().current_scene
 	world.add_child(grass_effect)
+	grass_effect.global_position = self.global_position
 	
 func _on_hurtbox_area_entered(_area:Area2D):
 	create_grass_effect()
