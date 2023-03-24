304576655c052333f105f8196c460d1d0fe190ca
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-03-23 19:08:18 -0700

    added rolling

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const ACCELERATION: int = 15
 const FRICTION: int = 15
 const MAX_SPEED: int = 150
+const ROLL_SPEED: int = 200
 
 @onready
 var animation_tree: AnimationTree = $AnimationTree
@@ -16,17 +17,23 @@ enum State {
 }
 
 var state: State = State.MOVE
+var roll_vector: Vector2 = Vector2.LEFT
 
 func _ready() -> void:
 	animation_tree.active = true
 
 func _process(_delta: float) -> void:
+	# start in move state
+	if Input.is_action_just_pressed("attack"):
+		state = State.ATTACK
+	if Input.is_action_just_pressed("roll"):
+		state = State.ROLL
+
 	match state:
 		State.MOVE:
 			move_state()
 		State.ROLL:
-			pass
-			# roll_state()
+			roll_state()
 		State.ATTACK:
 			attack_state()
 
@@ -37,9 +44,11 @@ func move_state() -> void:
 	input = input.normalized()
 
 	if input != Vector2.ZERO:
+		roll_vector = input # store direction player was facing
 		animation_tree.set("parameters/Idle/blend_position", input)
 		animation_tree.set("parameters/Run/blend_position", input)
 		animation_tree.set("parameters/Attack/blend_position", input)
+		animation_tree.set("parameters/Roll/blend_position", input)
 		animation_state.travel("Run")
 		self.velocity = self.velocity.move_toward(input * MAX_SPEED, ACCELERATION)
 		self.velocity.x = clamp(self.velocity.x, -1 * MAX_SPEED, MAX_SPEED)
@@ -50,12 +59,17 @@ func move_state() -> void:
 		
 	self.move_and_slide()
 
-	if Input.is_action_just_pressed("attack"):
-		state = State.ATTACK
-
 func attack_state() -> void:
 	animation_state.travel("Attack")
 
-func _on_animation_tree_animation_finished(_anim_name):
-	if state == State.ATTACK:
+func roll_state() -> void:
+	self.velocity = self.roll_vector * MAX_SPEED
+	self.move_and_slide()
+	animation_state.travel("Roll")
+
+func _on_animation_tree_animation_finished(anim_name):
+	if "roll" in anim_name:
+		self.velocity = self.velocity.move_toward(Vector2.ZERO, FRICTION)
+		state = State.MOVE
+	elif "attack" in anim_name:
 		state = State.MOVE
