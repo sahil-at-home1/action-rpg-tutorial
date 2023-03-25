c04a2450345e559e65fb0607c3ffe96801af358f
Sahil Sharma <sahilsharm1617@gmail.com>
2023-03-25 11:43:25 -0700

    added global singleton to assign collision layers/masks in code

---
+++ player_vars.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var collision_map: Dictionary = {}
+
+func _ready():
+	for i in range(10):
+		var layer_name: String = "layer_" + str(i)
+		var temp: Variant = ProjectSettings.get_setting("layer_names/2d_physics/layer_" + str(i))
+		if temp:
+			layer_name = temp
+			collision_map[layer_name] = i
--- sword_hitbox.gd
+++ sword_hitbox.gd
@@ -1,3 +1,7 @@
 extends Area2D
 
 @onready var knockback_vector = Vector2.ZERO
+
+func _ready():
+	# set up collision layer and masks
+	self.set_collision_layer_value(PlayerVars.collision_map["player_hitbox"], true)
