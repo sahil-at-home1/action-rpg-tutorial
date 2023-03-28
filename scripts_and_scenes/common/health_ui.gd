060c1d777f85d3993d16e1acff16644cf4db02e5
Sahil Sharma <46827078+sahil-at-home1@users.noreply.github.com>
2023-03-27 21:05:13 -0700

    first iteration of heart label

--- scripts_and_scenes/common/health_ui.gd
+++ scripts_and_scenes/common/health_ui.gd
@@ -1,20 +1,28 @@
-extends Label
+extends Control
 
 
-const MAX_HEARTS = 4
+@onready var max_hearts = 4: set=set_max_hearts
+@onready var hearts = 4: set=set_hearts
 
-@onready var hearts = 4:
-	set(value): 
-		hearts = clamp(value, 0, MAX_HEARTS) 
-	get:
-		return hearts
+func set_max_hearts(value):
+	max_hearts = max(1, value)
 
+func set_hearts(value: int):
+	hearts = clamp(value, 0, max_hearts) 
+	if label != null:
+		label.text = "HP = %s" % str(hearts)
+
+@onready var player_stats: Node = get_node("../y-sort node/not-grass/Player/player_stats")
+@onready var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player_stats.health_changed.connect(set_hearts)
+	max_hearts = player_stats.max_health
+	hearts = player_stats.health
+	label.text = "HP = %s" % str(max_hearts)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
--- scripts_and_scenes/common/stats.gd
+++ scripts_and_scenes/common/stats.gd
@@ -5,12 +5,14 @@ extends Node
 	get = get_health, set = set_health
 		
 signal health_depleted
+signal health_changed(value)
 
 func get_health() -> int:
 	return health
 
 func set_health(value: int) -> void:
 	health = value
+	health_changed.emit(health)
 	if health <= 0:
 		health_depleted.emit()
 
